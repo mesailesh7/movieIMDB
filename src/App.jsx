3f41d7942c4187e6bdcee72be2fea339fdfc03af
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-28 01:58:20 -0700

    Api Data Fetchi

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import Search from "./components/Search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 function App() {
+  const [searchTerm, setsearchTerm] = useState("");
+  const [errorMessage, seterrorMessage] = useState(null);
+
+  const fetchMovies = async () => {
+    try {
+    } catch (error) {
+      console.error(`Error fetch movies: ${error}`);
+      seterrorMessage("Error fetching Movies: Please try again");
+    }
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <main>
-        <div className="pattern">
-          <div className="wrapper">
-            <header>
-              <img src="./hero.png" alt="Hero Banner" />
-              <h1>
-                Find <span className="text-gradient">Movies</span> You'll Enjoy
-                with the Hassle
-              </h1>
-            </header>
-            <p>Search</p>
-          </div>
+        <div className="pattern" />
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              with the Hassle
+            </h1>
+            <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+          </header>
+          <section className="all-movies">
+            <h2>All Movies</h2>
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          </section>
         </div>
       </main>
     </>
