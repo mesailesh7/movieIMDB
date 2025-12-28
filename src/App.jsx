ba03a0875d0bb5195535fcd379a8c279ed3dab6a
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-28 01:24:48 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="pattern">
+          <div className="wrapper">
+            <header>
+              <img src="./hero.png" alt="Hero Banner" />
+              <h1>
+                Find <span className="text-gradient">Movies</span> You'll Enjoy
+                with the Hassle
+              </h1>
+            </header>
+            <p>Search</p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
