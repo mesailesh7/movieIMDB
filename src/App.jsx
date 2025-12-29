3f9bd4c0a45534b92706dfe0e4995a957c4f6b6c
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-28 23:30:19 -0700

    Data Fetch from the API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 import Search from "./components/Search";
+import Spinner from "./components/spinner";
 
 //Api
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -17,20 +18,36 @@ const API_OPTIONS = {
 function App() {
   const [searchTerm, setsearchTerm] = useState("");
   const [errorMessage, seterrorMessage] = useState(null);
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    seterrorMessage("");
+
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
+
       if (!response.ok) {
         throw new Error("Failed to fetch movies");
       }
+
       const data = await response.json();
+
+      if (data.Response == "False") {
+        throw new Error("Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
 
       console.log(data);
     } catch (error) {
       console.error(`Error fetch movies: ${error}`);
       seterrorMessage("Error fetching Movies: Please try again");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -53,7 +70,21 @@ function App() {
           </header>
           <section className="all-movies">
             <h2>All Movies</h2>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <p key={movie.id} className="text-white">
+                    {movie.title}
+                  </p>
+                ))}
+              </ul>
+            )}
+
+            {/* {errorMessage && <p className="text-red-500">{errorMessage}</p>} */}
           </section>
         </div>
       </main>
